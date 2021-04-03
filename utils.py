59423d3fcfdff009bd20d131c38c5eaac90ecd47
Eliott Nys <eliott.nys@gmail.com>
2021-04-03 12:49:41 +0200

    finished functions in utils.py

--- utils.py
+++ utils.py
@@ -2,26 +2,33 @@
 # Math library
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
+import math
 
-def fact(n):
-	"""Computes the factorial of a natural number.
-	
-	Pre: -
-	Post: Returns the factorial of 'n'.
-	Throws: ValueError if n < 0
-	"""
-	pass
+def fact(n):	#renvoie le factoriel de n
+	if n < 0:
+		resultat = ValueError
+	elif n == 0:
+		resultat = 1
+	else:
+		resultat = n
+		while n > 1:
+			n -= 1
+			resultat *= n
+	return resultat
 
-def roots(a, b, c):
-	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
-	
-	Pre: -
-	Post: Returns a tuple with zero, one or two elements corresponding
-		to the roots of the ax^2 + bx + c polynomial.
-	"""
-	pass
+def roots(a, b, c):		#renvoie les racines de l'equation ax^2 + bx + c = 0 sous forme de tuple
+	D = b**2 - 4*a*c
+	if D < 0:
+		return ()
+	if D == 0:
+		x = (-b + math.sqrt(D))/(2*a)
+		return (x)
+	if D > 0:
+		x1 = (-b + math.sqrt(D))/(2*a)
+		x2 = (-b - math.sqrt(D))/(2*a)
+		return (x1, x2)
 
-def integrate(function, lower, upper):
+def integrate(function, lower, upper):		#renvoie l'integrale de lower à upper de l'equation function
 	"""Approximates the integral of a fonction between two bounds
 	
 	Pre: 'function' is a valid Python expression with x as a variable,
@@ -34,6 +41,7 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
+	# I was unable to import the scipy mudule, further research necessary.
 	pass
 
 if __name__ == '__main__':
